926dab0ba075a2c1449a8610238f0f877f80357a
Ashikzzzz <ashik.emon5.im@gmail.com>
2023-03-27 01:58:49 +0600

    booking api done

--- utils/dbconnect.js
+++
@@ -1,7 +0,0 @@
-function dbconnect (){
-// const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.0zgm21v.mongodb.net/?retryWrites=true&w=majority`;
-// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-console.log("DB connect")
-}
-
-module.exports = dbconnect;
